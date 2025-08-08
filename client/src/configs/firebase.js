d20c019a256844f6b5c13f74dd61ceded207e441
StewartSidauruk <220711816@students.uajy.ac.id>
2025-08-08 10:55:52 +0700

    feat:make login, register page and routing

---
+++ client/src/configs/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMMr__lc9PUhJubyOncS0tX7jdKzE2EGk",
+  authDomain: "phase2-gc1.firebaseapp.com",
+  projectId: "phase2-gc1",
+  storageBucket: "phase2-gc1.firebasestorage.app",
+  messagingSenderId: "856835224042",
+  appId: "1:856835224042:web:e4e43143fb13b6733d8293",
+  measurementId: "G-3684ZTR958"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
